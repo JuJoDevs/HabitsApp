796cebac2b41b496659ed8221b898b95057b5f2b
Jujob <jujo.bonillo90@gmail.com>
2024-05-01 00:28:22 +0200

    version app to version catalogs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,12 +8,12 @@ plugins {
 
 android {
     namespace = "com.jujodevs.habitsappcourse"
-    compileSdk = 34
+    compileSdk = libs.versions.compileSdk.get().toInt()
 
     defaultConfig {
         applicationId = "com.jujodevs.habitsappcourse"
-        minSdk = 24
-        targetSdk = 34
+        minSdk = libs.versions.minSdk.get().toInt()
+        targetSdk = libs.versions.targetSdk.get().toInt()
         versionCode = 1
         versionName = "1.0"
 
